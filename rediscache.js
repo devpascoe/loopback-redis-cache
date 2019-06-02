a74203cd66e4371f011f78bf243a46b1e4980706
Steve Pascoe <steve@airsickness.com>
2019-06-02 22:10:18 +1000

    refer to redis singleton if one exists

--- rediscache.js
+++ rediscache.js
@@ -1,132 +1,138 @@
 module.exports = function(Model, options) {
-    if(options.client){
-        var clientSettings = options.client;
-    }else{
-        var app = require('../../server/server.js');
-        var clientSettings = app.get('redis');
-    }
+  var redis = require("redis")
+  var client = null
 
-    var redis = require("redis"),
-        client = redis.createClient(clientSettings);
+  if (options.client) {
+      var clientSettings = options.client;
+      client = redis.createClient(clientSettings);
+  } else {
+      var app = require('../../server/server.js');
+      var clientSettings = app.get('redis');
+      if (app.client) {
+          client = app.client
+      } else {
+          client = redis.createClient(clientSettings);
+      }
+  }
 
-    var redisDeletePattern = require('redis-delete-pattern');
+  var redisDeletePattern = require('redis-delete-pattern');
 
-    client.on("error", function (err) {
-        console.log(err);
-        // try to connect again with server config
-        if(err.toString().indexOf("invalid password") !== -1){
-            console.log("Invalid password... reconnecting with server config...");
-            var app = require('../../server/server');
-            var clientSettings = app.get('redis');
-            client = redis.createClient(clientSettings);
-        }
-    });
+  client.on("error", function (err) {
+      console.log(err);
+      // try to connect again with server config
+      if(err.toString().indexOf("invalid password") !== -1){
+          console.log("Invalid password... reconnecting with server config...");
+          var app = require('../../server/server');
+          var clientSettings = app.get('redis');
+          client = redis.createClient(clientSettings);
+      }
+  });
 
 
-    Model.beforeRemote('**', function(ctx, res, next) {
-        // get all find methods and search first in cache
-        if((ctx.method.name.indexOf("find") !== -1 || ctx.method.name.indexOf("__get") !== -1 || ctx.method.name.indexOf("get") !== -1) && client.connected){
-            if(typeof ctx.req.query.cache != 'undefined'){
-                var modelName = ctx.method.sharedClass.name + "_" + ctx.method.name;
-                var cachExpire = ctx.req.query.cache;
+  Model.beforeRemote('**', function(ctx, res, next) {
+      // get all find methods and search first in cache
+      if((ctx.method.name.indexOf("find") !== -1 || ctx.method.name.indexOf("__get") !== -1 || ctx.method.name.indexOf("get") !== -1) && client.connected){
+          if(typeof ctx.req.query.cache != 'undefined'){
+              var modelName = ctx.method.sharedClass.name + "_" + ctx.method.name;
+              var cachExpire = ctx.req.query.cache;
 
-                // set key name
-                var cache_key = modelName+'_'+new Buffer(JSON.stringify(ctx.req.params) + JSON.stringify(ctx.req.query)).toString('base64');
+              // set key name
+              var cache_key = modelName+'_'+new Buffer(JSON.stringify(ctx.req.params) + JSON.stringify(ctx.req.query)).toString('base64');
 
-                // search for cache
-                client.get(cache_key, function(err, val) {
-                    if(err){
-                        console.log(err);
-                    }
+              // search for cache
+              client.get(cache_key, function(err, val) {
+                  if(err){
+                      console.log(err);
+                  }
 
-                    if(val !== null){
-                        ctx.result = JSON.parse(val);
-                        ctx.res.setHeader('Cache-Hit', true)
-                        client.ttl(cache_key, function(err, data) {
-                          if (!err) {
-                            ctx.res.setHeader('Cache-TTL', data)
-                            var expires = new Date()
-                            expires.setSeconds(expires.getSeconds() + parseInt(data))
-                            ctx.res.setHeader('Expires', expires)
-                          }
-                          ctx.done(function (err) {
-                            if (err) return next(err);
-                          });
-                        })
-                    }else{
-                        //return data
-                        ctx.res.setHeader('Cache-Hit', false)
-                        ctx.res.setHeader('Cache-TTL', cachExpire)
-                        var expires = new Date()
-                        expires.setSeconds(expires.getSeconds() + parseInt(cachExpire))
-                        ctx.res.setHeader('Expires', expires)
-                        next();
-                    }
-                });
+                  if(val !== null){
+                      ctx.result = JSON.parse(val);
+                      ctx.res.setHeader('Cache-Hit', true)
+                      client.ttl(cache_key, function(err, data) {
+                        if (!err) {
+                          ctx.res.setHeader('Cache-TTL', data)
+                          var expires = new Date()
+                          expires.setSeconds(expires.getSeconds() + parseInt(data))
+                          ctx.res.setHeader('Expires', expires)
+                        }
+                        ctx.done(function (err) {
+                          if (err) return next(err);
+                        });
+                      })
+                  }else{
+                      //return data
+                      ctx.res.setHeader('Cache-Hit', false)
+                      ctx.res.setHeader('Cache-TTL', cachExpire)
+                      var expires = new Date()
+                      expires.setSeconds(expires.getSeconds() + parseInt(cachExpire))
+                      ctx.res.setHeader('Expires', expires)
+                      next();
+                  }
+              });
 
-            }else{
-                next();
-            }
-        }else{
-            next();
-        }
-    });
+          }else{
+              next();
+          }
+      }else{
+          next();
+      }
+  });
 
-    Model.afterRemote('**', function(ctx, res, next) {
-        // get all find methods and search first in cache - if not exist save in cache
-        if((ctx.method.name.indexOf("find") !== -1 || ctx.method.name.indexOf("__get") !== -1 || ctx.method.name.indexOf("get") !== -1) && client.connected){
-            if(typeof ctx.req.query.cache != 'undefined'){
-                var modelName = ctx.method.sharedClass.name + "_" + ctx.method.name;
-                var cachExpire = ctx.req.query.cache;
+  Model.afterRemote('**', function(ctx, res, next) {
+      // get all find methods and search first in cache - if not exist save in cache
+      if((ctx.method.name.indexOf("find") !== -1 || ctx.method.name.indexOf("__get") !== -1 || ctx.method.name.indexOf("get") !== -1) && client.connected){
+          if(typeof ctx.req.query.cache != 'undefined'){
+              var modelName = ctx.method.sharedClass.name + "_" + ctx.method.name;
+              var cachExpire = ctx.req.query.cache;
 
-                // set key name
-                var cache_key = modelName+'_'+new Buffer(JSON.stringify(ctx.req.params) + JSON.stringify(ctx.req.query)).toString('base64');
-                // search for cache
-                client.get(cache_key, function(err, val) {
-                    if(err){
-                        console.log(err);
-                    }
+              // set key name
+              var cache_key = modelName+'_'+new Buffer(JSON.stringify(ctx.req.params) + JSON.stringify(ctx.req.query)).toString('base64');
+              // search for cache
+              client.get(cache_key, function(err, val) {
+                  if(err){
+                      console.log(err);
+                  }
 
-                    if(val == null){
-                        // set cache key
-                        client.set(cache_key, JSON.stringify(res));
-                        client.expire(cache_key, cachExpire);
-                        next();
-                    }else{
-                        next();
-                    }
-                });
+                  if(val == null){
+                      // set cache key
+                      client.set(cache_key, JSON.stringify(res));
+                      client.expire(cache_key, cachExpire);
+                      next();
+                  }else{
+                      next();
+                  }
+              });
 
-            }else{
-                next();
-            }
-        }else{
-            next();
-        }
-    });
+          }else{
+              next();
+          }
+      }else{
+          next();
+      }
+  });
 
-    Model.afterRemote('**', function(ctx, res, next) {
-        // delete cache on patchOrCreate, create, delete, update, destroy, upsert
-        if((ctx.method.name.indexOf("find") == -1 && ctx.method.name.indexOf("__get") == -1) && client.connected){
-            var modelName = ctx.method.sharedClass.name + "_" + ctx.method.name;
-            var cachExpire = ctx.req.query.cache;
+  Model.afterRemote('**', function(ctx, res, next) {
+      // delete cache on patchOrCreate, create, delete, update, destroy, upsert
+      if((ctx.method.name.indexOf("find") == -1 && ctx.method.name.indexOf("__get") == -1) && client.connected){
+          var modelName = ctx.method.sharedClass.name + "_" + ctx.method.name;
+          var cachExpire = ctx.req.query.cache;
 
-            // set key name
-            var cache_key = modelName+'_*';
+          // set key name
+          var cache_key = modelName+'_*';
 
-            // delete cache
-            redisDeletePattern({
-                redis: client,
-                pattern: cache_key
-            }, function handleError (err) {
-                if(err){
-                    console.log(err);
-                }
-                next();
-            });
+          // delete cache
+          redisDeletePattern({
+              redis: client,
+              pattern: cache_key
+          }, function handleError (err) {
+              if(err){
+                  console.log(err);
+              }
+              next();
+          });
 
-        }else{
-            next();
-        }
-    });
+      }else{
+          next();
+      }
+  });
 }
